9293a540e16bbf966348197fec38264635297895
lduttonucsd <duttonaudioworks@gmail.com>
2019-04-23 11:01:52 -0700

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route path="/books/:id" componenet={Detail} />
+          <Route componenent={NoMatch} />
+          <Books />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
